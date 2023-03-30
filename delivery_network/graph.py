bb665983c5914e509c2585cbe3673c144e9b8c81
mbruneau <matis.bruneau@ensae.fr>
2023-03-30 07:42:28 +0000

    glutonny fonctionne

--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -309,7 +309,7 @@ def routes_test(graphe_path, route_path):
 Q10 : notre algorithme ne fonctionne pas sur les graphes au-delà du graphe 1, car python s'arrête à cause d'une boucle trop longue
 Q15 : en utilisant la fonction routes_test, notre programme estime mettre 110 minutes pour calculer les routes du fichier routes.2.in
 """
-def glutonny(self, path_routes_x, path_trucks_x, budget = 25e9):
+def glutonny(path_routes_x, path_trucks_x, budget = 25e9):
     
 #on fait utilité/cout [trajet, optimal, coût] et on trie la liste selon 3
 #on achète les camions de la liste jusqu'à atteindre le budget
@@ -351,19 +351,20 @@ def glutonny(self, path_routes_x, path_trucks_x, budget = 25e9):
 
     for trajet in liste_routes : # on ajoute le rapport utilité/cout à la liste des routes
         if trajet[2] != None :
-        trajet.append(trajet[2] / liste_camions[trajet[3]][1])
+            trajet.append(trajet[2] / liste_camions[trajet[3]][1])
         else :
             trajet.append(0)
     
-    liste_routes.sort(key = itemgetter(5)) # on trie la liste en fonction du rapport précédent
+    liste_routes.sort(key = itemgetter(4)) # on trie la liste en fonction du rapport précédent
 
     nb_routes = len(liste_routes)
     budget = 25e9
     index = 0
     achat_camion = []
-    while budget > 0 and (index <= len(liste_routes)) and (liste_routes[index+1][5] > 0): #tant que le budget n'est pas épuisé, qu'on a pas fait toutes les routes et que les prochaines routes sont faisables
-        achat_camion.append([liste_routes[index][3], liste_routes[index][0]]) # on ajoute à la liste des achats de camion un camion et son trajet
-        budget = budget - liste_camions[liste_routes[index][3]][1] # on l'enlève du budget
+    while budget > 0 and (index < len(liste_routes)): #tant que le budget n'est pas épuisé, qu'on a pas fait toutes les routes et que les prochaines routes sont faisables
+        if liste_routes[index][4] > 0:
+            achat_camion.append([liste_routes[index][3], liste_routes[index][0]]) # on ajoute à la liste des achats de camion un camion et son trajet
+            budget = budget - liste_camions[liste_routes[index][3]][1] # on l'enlève du budget
         index += 1 #on augmente l'index
 
     if budget < 0 : #si le dernier camion était trop cher on le "rend"
