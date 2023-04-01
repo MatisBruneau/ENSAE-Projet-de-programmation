fb7dbb0d1838e4db7fa4022a32bfd37482e8022b
mbruneau <matis.bruneau@ensae.fr>
2023-04-01 15:26:44 +0000

    edit

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -5,8 +5,8 @@ from time import perf_counter
 #data_path = "/home/onyxia/work/ENSAE-Projet-de-programmation/input/"
 graph_path = "/home/onyxia/ENSAE-Projet-de-programmation/input/network.6.in"
 route_path = "/home/onyxia/ENSAE-Projet-de-programmation/input/routes.6.in"
-route_1 = "/home/onyxia/ENSAE-Projet-de-programmation/output/route.1.out"
-camion_1 = "/home/onyxia/ENSAE-Projet-de-programmation/input/trucks.1.in"
+routes = "/home/onyxia/ENSAE-Projet-de-programmation/output/route.2.out"
+camions = "/home/onyxia/ENSAE-Projet-de-programmation/input/trucks.2.in"
 
 #routes(graph_path, route_path)
 #print("finish")
@@ -15,9 +15,8 @@ camion_1 = "/home/onyxia/ENSAE-Projet-de-programmation/input/trucks.1.in"
 #parents = k.dfs2()
 #print(k.saumon(parents, 2, 103))
 #print(g.min_power(2, 103))
-routes2(graph_path, route_path)
-
-
+#routes2(graph_path, route_path)
+print(glutonny(routes, camions))
 
 
 
